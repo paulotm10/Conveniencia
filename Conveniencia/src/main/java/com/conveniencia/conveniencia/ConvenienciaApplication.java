b1168b9413ee92bcd5535862a6c0e32273520959
Paulo Henrique <gouveia.paulo18@gmail.com>
2020-02-27 16:16:04 -0300

    primeiro commit

---
+++ Conveniencia/src/main/java/com/conveniencia/conveniencia/ConvenienciaApplication.java
@@ -0,0 +1,13 @@
+package com.conveniencia.conveniencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvenienciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvenienciaApplication.class, args);
+	}
+
+}
---
+++ Conveniencia/src/test/java/com/conveniencia/conveniencia/ConvenienciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conveniencia.conveniencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenienciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
